981d550f9c51c3bdedfe5c656f5622be04d62b70
Dawidson Pereira <dawidsongru@gmail.com>
2024-01-14 14:19:13 -0300

    Sistema de Celular (Desafio resolvido)

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando à partir da App Store o app {nomeApp} no iPhone {Modelo} ...");
+        }        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nSmartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "999999999", modelo: "G11", imei: "999999999", memoria: 128);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Waze");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone iPhone:");
+        Smartphone iphone = new Iphone(numero: "888888888", modelo: "15 Pro", imei: "888888888", memoria: 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("\n");
+    }
+}
